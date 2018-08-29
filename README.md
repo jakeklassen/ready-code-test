f491245149bb91508b8774e3273f9a459681708a
Jake Klassen <jklassendev@gmail.com>
2018-08-29 06:55:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ready Code Test
+
+See instructions at https://gist.github.com/aparrish/691b0301f6737d65b01db9920a60a0a5
+
+Written using Flask and Python 3.7.0
+
+## Comments
+
+I enjoyed messing with Python having never written it before (maybe in college :thinking:).
+
+Arbitrary test in [gist](https://gist.github.com/aparrish/691b0301f6737d65b01db9920a60a0a5#file-apitest-py-L18) requiring sorted result. Probably makes more sense to use something like [`assert.assertCountEqual()`](https://docs.python.org/3.2/library/unittest.html#unittest.TestCase.assertCountEqual) rather than forcing a server side sort without reason.
+
+If this were a production endpoint I would have loaded the csv file into a sqlite database since the data is static and leveraging SQL would be ideal. Loading the file into memory per request isn't ideal, but was quick for test purposes.
---
+++ main.py
@@ -0,0 +1,42 @@
+import csv
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+@app.route("/count")
+def count():
+    with open('dogs_of_nyc_ wnyc.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        query_params = list(map(lambda x: x.lower(), request.args.keys()))
+        invalid_params = set(query_params).difference(reader.fieldnames)
+
+        if (len(invalid_params) > 0):
+            unknown_fields = list(invalid_params)
+            unknown_fields.sort()
+
+            return jsonify({"unknown fields": unknown_fields}), 400
+
+        count = 0
+
+        for row in reader:
+            if all(row_match(key, value, row) for key, value in request.args.items()):
+                count += 1
+
+        return jsonify({"count": count})
+
+
+def row_match(key, value, row):
+    key = key.lower()
+
+    # Parse and compare `zip_code`
+    if key == 'zip_code':
+        query_zip = int(value)
+        row_zip = int(row[key])
+
+        if query_zip == row_zip:
+            return True
+    # The rest are strings
+    elif row[key].lower() == value.lower():
+        return True
+
+    return False
